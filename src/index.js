d2d2ddebc3638029818703419ab9bcab74a1371d
danie <danie@example.com>
2026-06-15 05:44:58 -0400

    last comit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const authRoutes = require('./routes/auth.routes');
 const checkoutRoutes = require('./routes/checkout.routes');
 const healthRoutes = require('./routes/health.routes');
+const swaggerRoutes = require('./routes/swagger.routes');
 
 const app = express();
 app.use(express.json());
@@ -9,6 +10,7 @@ app.use(express.json());
 app.use('/api', authRoutes);
 app.use('/api', checkoutRoutes);
 app.use('/api', healthRoutes);
+app.use('/api', swaggerRoutes);
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' });
--- src/routes/swagger.routes.js
+++ src/routes/swagger.routes.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../../swagger.json');
 const swaggerController = require('../controllers/swagger.controller');
 
 const router = express.Router();
 
 router.get('/swagger', swaggerController.getSwagger);
+router.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 module.exports = router;
